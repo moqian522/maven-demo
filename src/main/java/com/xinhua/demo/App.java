1fbbe2ac8282395c07a4be4b929c3c2b8c64f072
Heather <qian.mo@qq.com>
2021-09-17 17:48:51 +0800

    initial commit

---
+++ src/main/java/com/xinhua/demo/App.java
@@ -0,0 +1,14 @@
+package com.xinhua.demo;
+
+/**
+ * This is a {@link App} demo.
+ * @author Heather
+ */
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ src/test/java/com/xinhua/demo/AppTest.java
@@ -0,0 +1,12 @@
+package com.xinhua.demo;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testMain() {
+    App.main(null);
+  }
+
+}
