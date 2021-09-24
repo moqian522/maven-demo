6eaee0507f98b6d9893a881671ea147e65179183
Heather <qian.mo@qq.com>
2021-09-24 19:10:53 +0800

    add HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * This is a {@link HelloWorld} demo.
+ * @author Heather
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
